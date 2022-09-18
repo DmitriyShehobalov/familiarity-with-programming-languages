7837c2eadbe1903f74831b708d7e8bcc06bd5925
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-09-18 16:58:35 +0300

    Added Folder Pet

---
+++ Pet/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+
+if (username.ToLower()== "masha")
+{
+    Console.WriteLine("Ooh, it is Masha!");
+}
+else
+{
+    Console.WriteLine("Hi, " + username);
+}
