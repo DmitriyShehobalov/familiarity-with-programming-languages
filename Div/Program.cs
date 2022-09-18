9a3d0c3057c8df51659928b238f2176baafafbac
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-09-18 16:00:57 +0300

    Added Div folder

---
+++ Div/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter First Number");
+double numberA = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+double numberB = double.Parse(Console.ReadLine());
+
+double result = numberA / numberB;
+
+Console.WriteLine("Answer = " + result);
