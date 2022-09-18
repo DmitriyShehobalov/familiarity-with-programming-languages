ed2d1d9f42caf3a2bf6f5388bcc9d8d3da91103b
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-09-18 21:42:46 +0300

    Added Kettlebell folder

---
+++ Kettlebell/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 3;
+int b = 50;
+int c = 600;
+int d = 182;
+int e = 2;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine(max);
